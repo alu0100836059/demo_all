e954798218133a2de2f5ed47a67d7d620ee0b2a6
alu0100836059 <alu0100836059@ull.edu.es>
2016-12-31 01:04:19 +0000

    desplegando a github_apuntes

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('empujar-libro',
  shell.task("git add .; git commit -am 'desplegando a github_apuntes';"+
     // "git remote add repo_apuntes_no_tocar git@github.com:alu0100836059/Apuntes_SYTW.git;"+
     // "git push repo_apuntes_no_tocar master;",
-    "git remote add origin " + paquete.repository.url + ";" + "git push origin master;"
+    "git remote add origin " + paquete.repository.url + "; git remote -v;" + "git push origin master;"
 
     // cambiar la coma de arriba por un +  ,borrar esta línea y descomentar las de abajo
     // ";"+
